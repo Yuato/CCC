43f05822ea2d0ce070d5fec9dce22528d280ab65
Yuato <tonyyuan1010@gmail.com>
2023-10-05 21:56:48 -0400

    My solution

---
+++ 2023/J5.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+int searcher(char letter, std::string word){
+    if ()
+}
+
+int main(){
+    int R,C;
+    std::string word, row;
+    std::cin>>word>>R>>C;
+    std::vector<std::vector<char>>grid;
+    for (int i = 0; i<C; i++){
+        std::cin>> row;
+        std::vector<std::vector<char>>grid(row.begin(), row.end());
+    }
+}
