ea200f1e980cd0079613fe5205f0e98ac1859640
Yuato <tonyyuan1010@gmail.com>
2024-03-22 01:10:37 -0400

    2008 S3 work in progress

---
+++ 2008/S3.cpp
@@ -0,0 +1,18 @@
+//work in progress
+//Solution: BFS
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool visited [100][100];
+int arr [100][100];
+
+int main() {
+    int t; cin>>t;
+    for (int i = 0; i<t; i++){
+        int r, c;cin>>r>>c; 
+        for (int x = 0; x<r; x++){
+            for (int y = 0; y<c; y++)
+        }
+    }
+}
